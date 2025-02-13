f203a7ae2ea341bec1ec9e5b7d99dabd0ce979e0
unknown <asbaxter1@gmail.com>
2025-02-13 14:17:06 -0500

    testing with TLS off again

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const winston = require("winston");
 require("winston-daily-rotate-file");
 
 //REMOVE in production
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 // Configure console logging with the same format as file logging
 const consoleTransport = new winston.transports.Console({
